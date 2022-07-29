7fea20b4a047ca34db894bc22465a9341016cdef
Matan Ljubas <matan.ljubas@3ofthem.eu>
2022-07-29 11:23:23 +0200

    Latest

--- bin/server.dart
+++ bin/server.dart
@@ -1,87 +1,38 @@
-import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
-
-import 'api/app_data.dart';
-import 'api/provider.dart';
-import 'data/mingo.dart';
-
-final _router = Router()..get('/penalised-providers', _penalisedProviderHandler);
-
-Response _penalisedProviderHandler(Request req) {
-  return Response.ok(
-    jsonEncode(MinGOData.penalisedProviders.map((e) => e.toJson()).toList()),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*',
-      'Access-Control-Expose-Headers': '*',
-      'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, HEAD, OPTIONS',
-    },
-  );
-}
-
-// ignore: unused_element
-late Timer _cacheRefreshTimer;
-
-class InvalidSslOverride extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (
-        X509Certificate cert,
-        String host,
-        int port,
-      ) {
-        return true;
-      };
-  }
-}
-
-SecurityContext getSecurityContext() {
-  // Bind with a secure HTTPS connection
-  final chain = Platform.script.resolve('../mingo_middleware.pem').toFilePath();
-  final key = Platform.script.resolve('../server_key.pem').toFilePath();
-
-  return SecurityContext()
-    ..useCertificateChain(chain)
-    ..usePrivateKey(key, password: '8g{X7CPWg?1?');
-}
+import 'package:shelf_letsencrypt/shelf_letsencrypt.dart';
 
 void main(List<String> args) async {
-  final ip = InternetAddress.anyIPv4;
+  final domain = 'min-go.hr';
+  final domainEmail = 'info@min-go.hr';
+  final certificatesDirectory = '/etc/ssl/certs';
 
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final certificatesHandler = CertificatesHandlerIO(Directory(certificatesDirectory));
 
-  final port = int.parse(Platform.environment['PORT'] ?? '1612');
-  final server = await serve(handler, ip, port); //, securityContext: getSecurityContext());
-  print('Server listening on port ${server.port}');
+  final letsEncrypt = LetsEncrypt(certificatesHandler, production: false);
 
-  HttpOverrides.global = InvalidSslOverride();
+  final pipeline = const Pipeline().addMiddleware(logRequests());
+  final handler = pipeline.addHandler(_processRequest);
 
-  await AppDataApi.getAll();
-  print('Data received');
-  await ProvidersApi.setAllPricingInfo();
-  print('Pricing info received');
-
-  _cacheRefreshTimer = Timer.periodic(
-    const Duration(hours: 24),
-    (_) async {
-      try {
-        await AppDataApi.getAll();
-        print('Data received');
-        await ProvidersApi.setAllPricingInfo();
-        print('Pricing info received');
-      } catch (e) {
-        print('$e');
-        await AppDataApi.getAll();
-        print('Data received');
-        await ProvidersApi.setAllPricingInfo();
-        print('Pricing info received');
-      }
-    },
+  final servers = await letsEncrypt.startSecureServer(
+    handler,
+    domain,
+    domainEmail,
+    port: 1312,
+    securePort: 1612,
   );
+
+  final server = servers[0]; // HTTP Server.
+  final serverSecure = servers[1]; // HTTPS Server.
+
+  server.autoCompress = true;
+  serverSecure.autoCompress = true;
+
+  print('Serving at http://${server.address.host}:${server.port}');
+  print('Serving at https://${serverSecure.address.host}:${serverSecure.port}');
+}
+
+Response _processRequest(Request request) {
+  return Response.ok('Requested: ${request.requestedUri}');
 }
